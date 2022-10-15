08d7fe30036f45dffc2832ba2db7ab13fa82a0d9
Patricia Rodríguez <prlongoria@gmail.com>
2022-10-16 00:13:50 +0200

    challenge01 functions

---
+++ 02-Fundamentals-Part-2/starter/Challenge01.js
@@ -0,0 +1,59 @@
+// Coding Challenge #1
+// Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new
+// gymnastics discipline, which works differently.
+// Each team competes 3 times, and then the average of the 3 scores is calculated (so
+// one average score per team).
+// A team only wins if it has at least double the average score of the other team.
+// Otherwise, no team wins!
+// Your tasks:
+// 1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+// 2. Use the function to calculate the average for both teams
+// 3. Create a function 'checkWinner' that takes the average score of each team
+// as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner
+// to the console, together with the victory points, according to the rule above.
+// Example: "Koalas win (30 vs. 13)"
+// 4. Use the 'checkWinner' function to determine the winner for both Data 1 and
+// Data 2
+// 5. Ignore draws this time
+// Test data:
+// § Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+// § Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+// Hints:
+// § To calculate average of 3 values, add them all together and divide by 3
+// § To check if number A is at least double number B, check for A >= 2 * B.
+// Apply this to the team's average scores �
+// GOOD LUCK �
+
+// let dolphinsScore = (44 + 23 + 71) / 3;
+// let koalasScore = (65 + 54 + 49) / 3;
+// if (dolphinsScore >= (koalasScore * 2)) {
+//     console.log(`Dolphins score is ${dolphinsScore} so they are the Champions`);
+// } else if (koalasScore >= (dolphinsScore * 2)) {
+//     console.log(`Koalas score is ${koalasScore} so they are the Champions`)
+// }else {
+//     console.log("There is no Champion this year!");
+// }
+
+const calcAverage = (average1, average2, average3) => (average1 + average2 + average3) / 3;//función flecha
+
+// function calcAverage (average1, average2, average3) { //función normal
+//     let average = (average1 + average2 + average3) / 3;
+//     return average;
+// }
+const averageDolphins = (calcAverage(44, 23, 71));//al ser función flecha, debo declarar los valores de los parámetros después
+const averageKoalas = (calcAverage(65, 54, 49));
+console.log(`Dolphins average is ${averageDolphins}`);
+console.log(`Koalas average is ${averageKoalas}`);
+
+function checkWinner(Dolphins, Koalas) {//aquí meto los parámetros, y también en los if
+    if (Dolphins >= (Koalas * 2)) {
+             return `Dolphins score is ${Dolphins} so they are the Champions`;
+         } else if (Koalas >= (Dolphins * 2)) {
+             return `Koalas score is ${Koalas} so they are the Champions`;
+         }else {
+             return "There is no Champion this year!";
+         }
+}
+const winner = checkWinner(averageDolphins, averageKoalas);// y aquí ya meto los valores para esos parámetros y que declaré antes de la función
+console.log(winner);
+
---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,98 @@
+'use strict';
+//Debo activar el modo estricto para que sea más seguro mi código en Javascrip:
+//esa línea de activación del strict debe ser la primera de mi script o sino no funcionará
+//también podría activarlo sólo para un bloque de código, pero es más práctico ponerlo al
+//principio del script y que proteja todo el código de este archivo
+//Esto evita errores accidentales: prohibe hacer ciertas cosas y además, crea errores visibles en
+//determinadas situaciones en las que sin este modo de restricción, Javascript fallará sin que yo sepa que he cometido un error
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest) hasDriversLicense = true;//Si escribiera mal adrede la variable y en cosola aparece el error, sin embargo, si no pongo use strict no me aparecerá el error, sólo no ejecuta el código
+if(hasDriversLicense) console.log('I can drive!');
+
+//const interface = 'Audio';//Aquí me saltará un error porque la palabra interface es reservada, y el modo estricto me lo detecta y  me lo canta en consola
+
+//Functions:
+function logger() {//aquí no reservo espacio para valores porque sólo quiero que haga siempre lo mismo sin necesidad de parámetros a los que asignarles un valor cuando llame a esta función
+    console.log('My name is Jonas');//Esta función no retorna un valor, pro eso no le meto parámetros para luego asignarles un valor(constante o variable)
+}
+logger();//calling, running or invoking the function, si no hago esto, nunca se ejecutará esa función
+
+function fruitProcessor(apples, oranges) {//ahí en los () van los parámetros, estos reservan espacios vacíos a rellenar cuando llamemos a la función para que se ejecute y le metemos su valor en el paréntesis en ese momento
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+//Ahora debo llamarla para que se ejecute:
+fruitProcessor(5, 0);//esto imprime en consola solamente 5 0, pero no imprime el juice porque tengo un return y no imprime en consola
+
+const appleJuice = fruitProcessor(5, 0);//así lo meto en una constante
+console.log(appleJuice);//así imprime el return de la función fruitProcessor
+//O también puedo hacerlo así:
+console.log(fruitProcessor(5, 0));//Y esto me da el mismo resultado que las líneas 29 y 30
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+//const age1 = calcAge1(1990);//puedo declarar aquí la constante, antes de declarar la función, o la puedo declarar después, funcionará igualmente
+//Function declaration:
+function calcAge1(birthYear) {
+    const age = 2037 - birthYear;
+    //return age;
+    return 2037 - birthYear;
+}
+console.log(calcAge1(1991)); 
+
+//Function expression:(devuelve valor que guardo en una constante)
+const calcAge2 = function(birthYear) { //puedo meter una función en una variable
+    return 2037 - birthYear;
+}//en este caso, no puedo declarar la constante age2 antes ya que estoy metiendo la función en una constante, no funcionaría
+console.log(calcAge2(1991));
+
+const age2 = calcAge2(1991);
+const age1 = calcAge1(1990);//aquí la declaro después y veo que funciona igual que cuando la declaré antes de declarar la función, porque a esta función no la meto en una constante.
+console.log(age1, age2);//así imprimo el resultado de ambas funciones con los valores que le asigné a sus parámetros
+
+
+//const calcAge3 = function(birthYear) {
+//    return 2037 - birthYear;
+//}
+
+//función flecha: la expresión que pongo en linea 63 es el equivalente a la función de las lineas 58-60
+const calcAge3 = birthYear => 2037 - birthYear;//cojo el parámetro birthyear y después de la flecha le pongo el valor que quiero que tenga, y a continuación lo meto en una constante
+//no necesito ni llaves ni el return en la función flecha. Lo que sí que debo hacer es llamarla para que se ejecute:
+console.log (calcAge3(1992));//o de otra forma sería:
+const age3 = calcAge3(1992);
+console.log (age3);//imprime lo mismo que la línea 65
+
+const yearsUntilRetirement = birthYear => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    return retirement;
+}
+console.log(yearsUntilRetirement(1991));//1991 es el valor del parámetro birthyear que es lo que pongo justo antes de la flecha
+
+//si tuviera varios parámetros:
+const yearsUntilRetirement2 = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} retires in ${retirement} years`;
+}
+console.log(yearsUntilRetirement2(1991, 'Jonas'));
+console.log(yearsUntilRetirement2(1980, 'Bob'));//Veo que puedo poner los valores que quiera a los parámetros, las veces que quiera
+
+//Functions calling other functions:
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {//ahí en los () van los parámetros, estos reservan espacios vacíos a rellenar cuando llamemos a la función para que se ejecute y le metemos su valor en el paréntesis en ese momento
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+    const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`;
+    return juice;
+}
+console.log(fruitProcessor(2, 3));
+
+//
