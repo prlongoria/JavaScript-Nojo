bcdbfe7d06651b3825fd29dc6181107d305ff2c5
Patricia Rodríguez <prlongoria@gmail.com>
2022-10-24 09:36:37 +0200

    Challenge04

---
+++ 02-Fundamentals-Part-2/starter/Challenge04.js
@@ -0,0 +1,73 @@
+// Coding Challenge #4
+// Let's improve Steven's tip calculator even more, this time using loops!
+// Your tasks:
+
+// 1. Create an array 'bills' containing all 10 test bill values
+    let billsArray = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+// 2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+    let tipsArray = [];
+    let totalsArray = [];
+
+// 3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate
+// tips and total values (bill + tip) for every bill value in the bills array. Use a for
+// loop to perform the 10 calculations!
+    
+    for (let i = 0; i <= billsArray.length - 1; i++){
+//  const calcTip = function(bill) {
+//    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+//  };//esta función está declarada en challenge02 y por eso no necesito volver a declararla en este challenge04
+        
+        tipsArray.push(calcTip(billsArray[i]));
+        console.log(tipsArray);
+
+        totalsArray.push (tipsArray[i]+billsArray[i]);
+        console.log(totalsArray);
+
+    };
+    
+
+// Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+// Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the
+// tips and totals arrays �
+// Bonus:
+// 4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as
+// an argument. This function calculates the average of all numbers in the given
+// array. This is a difficult challenge (we haven't done this before)! Here is how to
+// solve it:
+// 4.1. First, you will need to add up all values in the array. To do the addition,
+// start by creating a variable 'sum' that starts at 0. Then loop over the
+// array using a for loop. In each iteration, add the current value to the
+// 'sum' variable. This way, by the end of the loop, you have all values
+// added together
+// 4.2. To calculate the average, divide the sum you calculated before by the
+// length of the array (because that's the number of elements)
+// 4.3. Call the function with the 'totals' array
+// GOOD LUCK �
+
+let arr = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];//pongo este como ejemplo, pero puedo meterle lo que quiera 
+
+let sum = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    // console.log (sum); 
+};
+
+function calcAverage2() {
+    let totalAverage = sum / arr.length;
+    return (totalAverage);
+};
+
+console.log(calcAverage2(arr));
+
+// const calcAverage2 = function(arr) {
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         sum += arr[i]; //sum = sum + arr[i];
+//     };
+//     console.log (sum); 
+
+// }
+//calcAverage2(arr);
+
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -176,57 +176,57 @@
 // }
 
 //Objetos:
-const jonasArray = [
-    'Jonas', 
-    'Schmedtmann',
-    2037 - 1991,
-    'teacher',
-    'Michael', 'Peter', 'Steven'
-];
+// const jonasArray = [
+//     'Jonas', 
+//     'Schmedtmann',
+//     2037 - 1991,
+//     'teacher',
+//     'Michael', 'Peter', 'Steven'
+// ];
 
-const jonasObject = {//aqui meto las propiedades del objeto jonasObject
-    firstName: 'Jonas',
-    lastName: 'Schmedtmann',
-    age: 2037-1991,
-    job: 'teacher',
-    friends: ['Michael', 'Peter', 'Steven']
-};
-console.log(jonasObject);
+// const jonasObject = {//aqui meto las propiedades del objeto jonasObject
+//     firstName: 'Jonas',
+//     lastName: 'Schmedtmann',
+//     age: 2037-1991,
+//     job: 'teacher',
+//     friends: ['Michael', 'Peter', 'Steven']
+// };
+// console.log(jonasObject);
 
 //Obtener propiedades del objeto:
-console.log(jonasObject.lastName);//esta es una forma
-console.log(jonasObject['lastName']);//otra forma
+// console.log(jonasObject.lastName);//esta es una forma
+// console.log(jonasObject['lastName']);//otra forma
 
-const nameKey = 'Name';//creo una key para el Name de las propiedades del objeto
-console.log(jonasObject['first' + nameKey]);//y así consigo que imprima firstName
-console.log(jonasObject['last' + nameKey]);//y así el lastName
+// const nameKey = 'Name';//creo una key para el Name de las propiedades del objeto
+// console.log(jonasObject['first' + nameKey]);//y así consigo que imprima firstName
+// console.log(jonasObject['last' + nameKey]);//y así el lastName
 
-const interesetedIn =prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job and friends');
-//asdfconsole.log(jonasObject[interesetedIn]);//así me imprimirá lo que el usuario meta en el prompt
-if(jonasObject[interesetedIn]) {
-    console.log(jonasObject[interesetedIn]);
-} else {
-    console.log('Wrong request! Choose between firstName, lastName, age, job and friends');
-};
+// const interesetedIn =prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job and friends');
+// //asdfconsole.log(jonasObject[interesetedIn]);//así me imprimirá lo que el usuario meta en el prompt
+// if(jonasObject[interesetedIn]) {
+//     console.log(jonasObject[interesetedIn]);
+// } else {
+//     console.log('Wrong request! Choose between firstName, lastName, age, job and friends');
+// };
 
 //Puedo añadir más propiedades a mi objeto, así:
-jonasObject.location = 'Portugal';//puedo añadirlo con .
-jonasObject['twitter'] = '@jonasschmedtman';//puedo añadirlo con []
-console.log(jonasObject);//imprime el jonasObject con location y su twitter
+// jonasObject.location = 'Portugal';//puedo añadirlo con .
+// jonasObject['twitter'] = '@jonasschmedtman';//puedo añadirlo con []
+// console.log(jonasObject);//imprime el jonasObject con location y su twitter
 
 //Challenge: Jonas has 3 friends, and his best friend is called Michael:
-console.log(jonasObject.firstName + ' has ' +  jonasObject.friends.length + ' friends' + ' and his best friend is called '+ jonasObject.friends[0]);//esta es una forma
+// console.log(jonasObject.firstName + ' has ' +  jonasObject.friends.length + ' friends' + ' and his best friend is called '+ jonasObject.friends[0]);//esta es una forma
 
-console.log(`${jonasObject.firstName} has ${jonasObject.friends.length} friends, and his best friend is called ${jonasObject.friends[0]}`);//otra forma
+// console.log(`${jonasObject.firstName} has ${jonasObject.friends.length} friends, and his best friend is called ${jonasObject.friends[0]}`);//otra forma
 
 //Métodos para objetos:
-const jonasObject2 = {//aqui meto las propiedades del objeto jonasObject
-    firstName: 'Jonas',
-    lastName: 'Schmedtmann',
-    birthYear2: 1991,
-    job: 'teacher',
-    friends: ['Michael', 'Peter', 'Steven'],
-    hasDriversLicense: false,
+// const jonasObject2 = {//aqui meto las propiedades del objeto jonasObject
+//     firstName: 'Jonas',
+//     lastName: 'Schmedtmann',
+//     birthYear2: 1991,
+//     job: 'teacher',
+//     friends: ['Michael', 'Peter', 'Steven'],
+//     hasDriversLicense: false,
 
     //Puedo crear un método para este objeto, así:(aquí dentro no puedo meter una función). esto es pues, un valor de una función
     // calcAge5: function(birthYear) {
@@ -240,15 +240,15 @@ const jonasObject2 = {//aqui meto las propiedades del objeto jonasObject
     // }
 
     //Pero la mejor práctica es hacerlo de la siguiente manera, porque así lo meto en una nueva propiedad del objeto, es decir, añado una nueva propiedad con un método:
-    calcAge: function() {
-        this.age = 2037 - 1991;
-        return this.age;
-    },
+//     calcAge: function() {
+//         this.age = 2037 - 1991;
+//         return this.age;
+//     },
 
-    getSummary: function() {
-        return `${this.firstName} is a ${this.calcAge()} years old ${jonasObject2.job} and he has ${this.hasDriversLicense ? 'a' : 'no'} drivers license`;
-    }
-};
+//     getSummary: function() {
+//         return `${this.firstName} is a ${this.calcAge()} years old ${jonasObject2.job} and he has ${this.hasDriversLicense ? 'a' : 'no'} drivers license`;
+//     }
+// };
 
 //En lugar del método, podría crear una función fuera del objeto, talque así:
 //const calcAge5 = function(birthYear) {
@@ -260,8 +260,8 @@ const jonasObject2 = {//aqui meto las propiedades del objeto jonasObject
 
 //console.log(jonasObject2.calcAge());
 
-console.log(jonasObject2.calcAge());//debo llamar al método si quiero que luego me imprima la propiedad que añadí nueva, porque sino esa nueva propiedad aún no existe
-console.log(jonasObject2.age);//después de llamar al método ya puedo pedir la nueva propiedad
+// console.log(jonasObject2.calcAge());//debo llamar al método si quiero que luego me imprima la propiedad que añadí nueva, porque sino esa nueva propiedad aún no existe
+// console.log(jonasObject2.age);//después de llamar al método ya puedo pedir la nueva propiedad
 
 //Challenge: Jonas is a 46 year old teacher and he has a/no drivers's license
 
@@ -273,8 +273,115 @@ console.log(jonasObject2.age);//después de llamar al método ya puedo pedir la 
 // }
 
 // O también con operador ternario:
-console.log( `${jonasObject2.firstName} is a ${jonasObject2.age} years old ${jonasObject2.job} and he has ${jonasObject2.hasDriversLicense ? 'a' : 'no'} drivers license`);
+// console.log( `${jonasObject2.firstName} is a ${jonasObject2.age} years old ${jonasObject2.job} and he has ${jonasObject2.hasDriversLicense ? 'a' : 'no'} drivers license`);
 
 
 //O puedo hacerlo añadiendo el método getSummary al jonasObject, que es mejor práctica
-console.log(jonasObject2.getSummary());
+// console.log(jonasObject2.getSummary());
+
+//For Loop: se ejecuta mientras la condición es TRUE
+
+// for (let repetition = 1; repetition <= 10; repetition++) {
+// //declaro variable repetition igual a 1 porque es mi primera repetición desde la que quiero que empiece a realizar el loop, y esta será actualizada por el bucle for; condición lógica que se evalúa antes de cada iteración del ciclo y si esta es true, se ejecutará la siguiente iteración del ciclo y en cuanto esta condición sea falsa, el ciclo se detiene. EN ESTE CASO QUIERO QUE SE REPITA 10 VECES; actualizo el contador en 1, para que vaya repitiéndose hasta que deje de ser true la condición
+// console.log(`Lifting weights repetition ${repetition}`);//asi me lo imprime las 10 veces que quiero y cambia el número de cada repetición
+// }
+
+//Bucle for en un array:
+// const jonasArray = [
+//     'Jonas', 
+//     'Schmedtmann',
+//     2037 - 1991,
+//     'teacher',
+//     ['Michael', 'Peter', 'Steven'],
+//     true
+// ];
+
+// const types = [];//puedo crear un array vacío para llenarlo dentro del ciclo for
+
+// //la i debe ser 0 porque quiero recorrerlo desde el primer elemento del array, al cual llamo i:
+// for (let i = 0; i < jonasArray.length; i++) {//fijarme que he puesto i<.length, si pusiera <= debería poner .length-1
+//     console.log(jonasArray[i], typeof jonasArray[i]);//para que me imprima el array y los tipos de variables que lo componen
+
+//    //Puedo llenar el array vacío así:
+//     // types[i] = typeof jonasArray[i];//así lleno el array vacío
+
+//     //Hay otra manera de rellenar un array vacío:método push:ES MEJOR HÁBITO
+//     types.push(typeof jonasArray[i]);
+// };
+// console.log(types);
+
+// const years = [1991, 2007, 1969, 2020];//array con años de nacimiento
+// const ages = [];
+// for (let i = 0; i < years.length; i++) {
+//     ages.push(2037 - years[i]);//realiza la operación para conocer la edad y rellenar con esas cantidades el array vacío, suponiendo que 2037 es el año actual
+// };
+// console.log(ages);
+
+// //continue and break:
+
+// console.log('---ONLY STRINGS');
+// for (let i = 0; i < jonasArray.length; i++) {
+
+//     if(typeof jonasArray[i] != 'string') continue;//así le digo que sólo quiero que me registre los string en la consola
+//     console.log(jonasArray[i], typeof jonasArray[i]);
+// }
+// console.log('---FINISHED STRINGS');
+
+// console.log('---BREAK WITH NUMBER');
+// for (let i = 0; i < jonasArray.length; i++) {
+//     if(typeof jonasArray[i] === 'number') break;//así le digo que cuando encuentre un elemento que sea tipo número, detenga el ciclo
+//     console.log(jonasArray[i], typeof jonasArray[i]);//sólo imprime lo anterior al primer número que encuentra, de hecho, el número no lo imprime
+// }
+// console.log('---FINISHED BREAK WITH NUMBER');
+
+//looping backwards:
+// const jonasArray = [
+//     'Jonas', 
+//     'Schmedtmann',
+//     2037 - 1991,
+//     'teacher',
+//     ['Michael', 'Peter', 'Steven'],
+//     true
+// ];
+
+// for (let i = jonasArray.length - 1; i >= 0; i--) {//quiero que recorra el array al revés
+//     console.log(i, jonasArray[i]);//imprime los elementos del array al revés, y como le puse que imprima i delante, imprime el índice de cada uno
+// };
+
+// //loop in loop:
+// for(let exercise = 1; exercise <= 3; exercise++) {//así le digo que repita lo que voy a poner dentro de {}las veces que yo quiera, en este caso, 3 veces
+//     console.log(`----STARTING EXERCISE ${exercise}`);//imprime esto 3 veces y le cambia el número incrementándolo cada vez
+
+//     //Meto ahora en cada uno de los starting exercise el siguiente ciclo:
+//     for  (let repetition = 1; repetition <= 5; repetition++) {
+//         console.log(`Exercise ${exercise}: Lifting weight repetition ${repetition}`);
+//     };
+// }
+
+
+//While loop:SE PUEDE UTILIZAR EN MÁS SITUACIONES QUE EL FOR porque no necesita un contador salvo que yo lo quiera. Lo único que necesita el while es una condición que mientras sea verdadera siga el ciclo. Puedo usarlo si no necesito que un número se incremente, solo si quiero que pase algo, una condición
+// for  (let repetition = 1; repetition <= 10; repetition++) {
+//              console.log(`Lifting weight repetition ${repetition}`);
+// };
+
+// let repetition = 1;//debo declarar la variable fuera del while
+// while (repetition <= 10) {
+//     console.log(`WHILE LOOP: Lifting weight repetition ${repetition}`);//aquí pongo lo que quiero que haga mientras la condición repet<=10 sea true
+//     repetition++;//mientras sea true también va aumentando el contador
+// };
+
+//Ejemplo de tirar un dado: con un número ramdom (aleatorio)
+// let dice = Math.random() * 6;//me dará números decimales aleatorios entre 0 y 6
+// console.log(dice);//cada vez que acutalizo la página me imprime un número decimal aleatorio
+
+// let dice2 = Math.trunc(Math.random() * 6);//así me dará números aleatorios enteros entre 0 y 5
+// console.log(dice2);
+
+let dice3 = Math.trunc(Math.random() * 6) + 1;//así me dará números aleatorios enteros entre 1 y 6
+//console.log(dice3);
+
+while (dice3 !== 6) { //si dice es diferente de 6, el ciclo se ejecutará
+    console.log(`You rolled a ${dice3}`);//hasta aquí sólo estoy creando un número random entre 1 y 6 y eso me rompe el navegador pues sería un ciclo infinito, así que debo seguir creando nuevos valores de números random, y para ello debo meter dice en el loop:
+    dice3 = Math.trunc(Math.random() * 6) + 1;//así sigo generando números random hasta que salgo el seis, que es la condición que puse al while
+};
+//IMPROTANTE: CUANDO SEPA CUÁNTAS VECES SE VA A REPETIR EL LOOP, ENTONCES SÍ QUE NECESITO UN CONTADOR, por ejemplo cuando quiero recorrer un array, y para eso es mejor el loop for.
