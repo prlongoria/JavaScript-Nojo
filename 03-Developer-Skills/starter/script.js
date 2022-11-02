1d1d81b356e15642f780d554af0d491662df3311
Patricia Rodríguez <prlongoria@gmail.com>
2022-11-02 08:11:22 +0100

    how to solve problems

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -3,7 +3,7 @@
 
 const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
-//PROBLEM: Calculate the temperature amplitude (sometimes threre might be a sensor error)
+//PROBLEM1: Calculate the temperature amplitude (sometimes threre might be a sensor error)
 
 //To undesrstand the problem:
 
@@ -25,6 +25,8 @@ const calcTempAmplitude = function(temperatures){
     for(let i = 0; i < temperatures.length; i++)  {
         const currentTemperature = temperatures[i];
 
+        if (typeof currentTemperature != 'number') continue;
+
         if(currentTemperature > max){//si un elemento del array es mayor que el primero
          max = currentTemperature;//la variable max será igual a ese elemento del array
         }
@@ -33,6 +35,47 @@ const calcTempAmplitude = function(temperatures){
         }
     }
         console.log(max, min);//imprime la variable max, que es el mayor de los elementos y la min
+        return max - min;
 };  
 
-calcTempAmplitude(temperatures);//imprime el mayor y el menor de los elementos del array temperatures, pero puedo poner un array que yo quiera o meter directamente valores entre[]
+const amplitude = calcTempAmplitude(temperatures);//imprime el mayor y el menor de los elementos del array temperatures, pero puedo poner un array que yo quiera o meter directamente valores entre[]
+console.log(amplitude);
+
+//PROBLEM2: funcion should receive 2 arrays of temperatures:
+
+//To undesrstand the problem:
+//1.- with 2 arrays, should we implement funtionality twice? NO, just merge 2 arrays
+
+//To break the problem into sub-problems:
+//1.- How to merge 2 arrays? -buscar en google y encuentro: concat y push
+
+
+const calcTempAmplitude2 = function(temperature1, temperature2){
+    // const array1 = [];
+    // const array2 = [];
+    // const array3 = array1.concat(array2); 
+    
+    const temperatures = temperature1.concat(temperature2);
+    console.log(temperatures);
+
+    let max = temperatures[0];
+    let min = temperatures[0];
+
+    for(let i = 0; i < temperatures.length; i++)  {
+        const currentTemperature = temperatures[i];
+
+        if (typeof currentTemperature != 'number') continue;
+
+        if(currentTemperature > max){
+         max = currentTemperature;
+        }
+        if (currentTemperature< min) {
+            min = currentTemperature;
+        }
+    }
+        console.log(max, min);
+        return max - min;
+};  
+
+const amplitude2 = calcTempAmplitude2([3, 5, 1], [9, 0, 5]);
+console.log(amplitude2);
